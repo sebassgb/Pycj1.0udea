a64017dd7a231c2a699135a6ed7b48b50a2d2a0c
cristian <cristian.marind@udea.edu.co>
2017-05-12 11:51:38 -0500

    Por mejorar

--- public/js/controladoresBD/controladorEventoBD.js
+++ public/js/controladoresBD/controladorEventoBD.js
@@ -1,4 +1,21 @@
-var refe = database.ref('Eventos/futbol/ninja@ni com 11 05 2017 1 35/informacion');
-refe.update({
-	nombre:"asdsa"
-});
+//var refe = database.ref('Eventos/futbol/ninja@ni com 11 05 2017 1 35/informacion/participante');
+
+
+
+function suscribirUsuarioEvento(pkUsuario, evento){
+	var referencia = "Eventos/"+evento.deporte+"/";
+	referencia = referencia+JsonToPkEvento(evento);
+	var eventoActual = retornarJsonReferencia(referencia);
+	
+}
+
+function retornarJsonReferencia(refStr){
+	var referencia = database.ref(refStr);
+	referencia.on('value', function(snapshot) {
+  		return snapshot.val();
+	});
+}
+
+var eventoz = retornarJsonReferencia('Eventos/futbol/ninja@ni com 11 05 2017 1 35/informacion/participante');
+alert(eventoz);
+//alert(JsonToPkEvento(evento));
