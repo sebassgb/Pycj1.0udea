f4395c4efad18a1c41c519e47aa220e77cc8399a
Cristian Marin Durango <cristian.marind@udea.edu.co>
2017-05-13 12:05:59 -0500

    Callback Pri2

--- public/js/controladoresBD/controladorEventoBD.js
+++ public/js/controladoresBD/controladorEventoBD.js
@@ -7,6 +7,9 @@ function usarValidacionRelacionEvento(pkUsuario, evento, callback){
 function suscribirUsuarioEvento(pkUsuario, evento){
 	var referencia = "Eventos/"+evento.deporte+"/";
 	referencia = referencia+JsonToPkEvento(evento)+"/informacion";
+	//Esta linea es equivalente a decir result = el json de la referencia dada
+	//todo lo que sigue que dependa del json referencia debe de estar dentro de
+	//la funcion callback
 	usarJsonReferencia(referencia, function(value, result){
 		if(result.cuposTotales > result.cuposLlenos){
 			var newEvento = result;
@@ -19,16 +22,19 @@ function suscribirUsuarioEvento(pkUsuario, evento){
 	});
 }
 
-function updateReferencia(refStr, json){
-	var referencia = database.ref(refStr);
+function updateReferencia(refStr, json){//Actualiza la bd en la referencia = "refStr" con
+	var referencia = database.ref(refStr);//la informacion = "json"(solo actualiza, no borra lo que no este en el json)
 	referencia.update(json);
 }
 
-
+//Metodo para usar una referencia de la bd
+//referencia(Direccion) = "refStr", callback : Funcion donde se usara la referencia de la bd
+//el metodo callback tiene la siguiente forma function(value, result){...}
+//en este ejemplo se envia la referencia con el parametro result
 function usarJsonReferencia(refStr, callback){
 	var referencia = database.ref(refStr);
-	referencia.on('value', function(snapshot) {
-		callback(0, snapshot.val());
+	referencia.on('value', function(snapshot) {//Funcion asincrona
+		callback(0, snapshot.val());//Se ejecuta callback para usar la referencia cuando se tenga
 	});
 }
 
