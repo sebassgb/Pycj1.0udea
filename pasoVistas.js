bbe1a97984aa74a4db94b06702def01ba101671d
Cristian Marin Durango <cristian.marind@udea.edu.co>
2017-05-14 18:42:04 -0500

    Enlace localhost

--- pasoVistas.js
+++ pasoVistas.js
@@ -30,3 +30,23 @@ app.get('/perfil', function (req, res) {
    // Prepare output in JSON format
    res.sendFile( __dirname + "/public/html/drawer/" + "Perfil.html" ); 
 });
+
+app.get('/creditos', function (req, res) {
+   // Prepare output in JSON format
+   res.sendFile( __dirname + "/public/html/drawer/" + "creditos.html" ); 
+});
+
+app.get('/recuperacionContraseña', function (req, res) {
+   // Prepare output in JSON format
+   res.sendFile( __dirname + "/public/html/login/" + "RecuperacionContraseña.html" ); 
+});
+
+app.get('/registro', function (req, res) {
+   // Prepare output in JSON format
+   res.sendFile( __dirname + "/public/html/login/" + "Registrarme.html" ); 
+});
+
+app.get('/creacionEvento', function (req, res) {
+   // Prepare output in JSON format
+   res.sendFile( __dirname + "/public/html/barraInferior/evento/" + "creaEvento.html" ); 
+});
